8dbab8b07685b6b91ccbf9013d2f929b9488d17e
Peyton C <peyton.castill0@icloud.com>
2022-09-02 20:59:35 -0400

    started work on the manager prompts

---
+++ dev/prompt.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const inquirer = require('inquirer');
+//WHEN I start the application 
+//THEN I am prompted to enter the team manager’s name, CHECK employee ID, CHECK email address, office number
+// use inquirer to write these prompts maybe put the prompts inside there own js file.
+
+function managerPrompts() {
+
+
+    inquirer.prompt([
+        {
+          //team managers name
+          type: 'input',
+          message: `please enter your team manager name`,
+          name: 'manager-name'
+        },
+        {
+            //employee ID 
+            type: 'input',
+            message: `please enter your employee ID`,
+            name: 'manager-emp-ID'
+        },
+        {
+            //manager email
+            type: 'input',
+            message: `please enter your email address`,
+            name: 'manager-email'
+        },
+        {
+            //office number
+            type: 'input',
+            message: `please enter your office number`,
+            name: 'manager-office-num'
+        },
+      ])
+      .then((data) => 
+      {
+        console.log(data)
+        //we might neeed this soon idk
+      })
+}
+
+module.exports = managerPrompts()
---
+++ dev/tests/prompt.test.js
@@ -0,0 +1,10 @@
+
+const managerPrompts = require("../prompt")
+
+test('Testing if managerPrompts() is working manual input required.', () => {
+  running = undefined
+  
+  expect(managerPrompts).toEqual(running)
+
+}
+)
